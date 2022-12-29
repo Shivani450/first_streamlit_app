80731397fb6a0b550f83c8c0675fdfdbe509b3ac
Shivani450 <96415738+Shivani450@users.noreply.github.com>
2022-12-29 18:50:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,15 +24,15 @@ fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 streamlit.text(fruityvice_response)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + "watermelon")
 
-fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+#fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+#streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice=streamlit.text_input('What fruit would you like information about?','apple') 
-streamlit.write('The user entered',fruit_choice) 
-import requests
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+#fruit_choice=streamlit.text_input('What fruit would you like information about?','apple') 
+#streamlit.write('The user entered',fruit_choice) 
+#import requests
+#fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
