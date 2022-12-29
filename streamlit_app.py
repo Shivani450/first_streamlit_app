511096826483150d3926c7b9391b908f3b4c1b08
Shivani450 <96415738+Shivani450@users.noreply.github.com>
2022-12-29 19:08:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,9 +23,9 @@ import requests
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + "watermelon") 
 streamlit.text(fruityvice_response)
 
-streamlit.header("Fruityvice Fruit Advice!")
-import requests
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+#streamlit.header("Fruityvice Fruit Advice!")
+#import requests
+#fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #streamlit.text(fruityvice_response.json())
 
 
